93cdc38182941b966be6789d6cbdfc4728e46d46
Anastasya <aamokeeva@yandex.ru>
2024-01-18 15:45:42 +0300

    Создает модели, миграции, фабрики и наполняет базу фейковыми данными

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => ucfirst(fake()->words(2, true)),
+            'thumbnail' => $this->faker->dummyimage('products'),
+            'price' => fake()->numberBetween(1000, 10000),
+            'brand_id' => Brand::query()->inRandomOrder()->value('id')
+        ];
+    }
+}
