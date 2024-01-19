7d21dd2e581a05e6bc5d19a13fdf64537bcff0eb
Anastasya <aamokeeva@yandex.ru>
2024-01-19 17:15:08 +0300

    Подключает статику в layout (fonts, images), создает сервис провайдер для изображений

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <style>
-        .bt-icons i{
-            font-size: 30px;
-        }
-    </style>
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-<main class="container">
+@section('content')
     <div class="flex justify-center">
         <h1 class="mb-2">Install Bootstrap in Laravel with CoderAdvise.com</h1>
     </div>
-    <div class="bt-icons">
+    <div class="bt-icons fs-1">
         <i class="bi bi-airplane-fill"></i>
         <i class="bi bi-apple"></i>
         <i class="bi bi-balloon-heart"></i>
-        <i class="bi bi-alarm-fill"></i>
+        <i class="bi bi-alarm-fill text-success"></i>
     </div>
-</main>
-</body>
-</html>
+
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="{{ Vite::image('hedgehog.png') }}" class="d-block mx-lg-auto img-fluid"
+                     alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+                    popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
+                    extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
